9726ce39650a306be77b495ef4a4ca58c531a2e8
Maria Fernanda <ferdinandamoge@gmail.com>
2022-03-16 16:58:02 -0300

    teste commit

--- src/components/TesteEvento.vue
+++ src/components/TesteEvento.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="Teste-Evento">
-        <h2> Teste Evento</h2>
+        <h2> Teste Evento!</h2>
         <p @click="deixarMaiuscula">{{ mensagem }}</p>
     </div>
 </template>
